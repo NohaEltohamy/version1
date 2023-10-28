7cb809134897cf6781170ba6a1c9eb3c6a9fe8a6
Noha Salah <nohasalah@iti.gov.eg>
2023-10-28 13:47:19 +0200

    conf

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function myFunction() {
   alert ("Hello World!");
 }
 console.log("Text");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  alert ("Hello World!");
+}
+console.log("Text");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  alert ("Hello World!");
+}
+console.log("Text");
